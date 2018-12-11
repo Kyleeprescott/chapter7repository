7ac06d3fc7017fe2c9415303c05161acca14c7e8
kylee.prescott <kylee.prescott@CLC-LAB209-09.TCSD.tooelesd.org>
2018-12-11 14:03:54 -0700

    work

---
+++ src/examples7/BuildClass.java
@@ -0,0 +1,25 @@
+package examples7;
+
+public class BuildClass {
+
+	public static void main(String[] args) {
+		ClassExamples myacc = new ClassExamples();
+		ClassExamples youracc = new ClassExamples();
+		myacc.name = "Kylee Prescott";
+		youracc.name = "Erik BroadBent";
+		myacc.address = "423E 1370N";
+		 youracc.address = "1354 W 245N ";
+		 myacc.balance = 25000000;
+		  youracc.balance = .25;
+		  myacc.phone = "4359466929";
+		  youracc.phone = "8015205571";
+		  System.out.println(myacc.name);
+		  System.out.println(myacc.address);
+		  System.out.println(myacc.phone);
+		  System.out.println(myacc.balance );
+		  
+		  
+
+	}
+
+}
---
+++ src/examples7/ClassExamples.java
@@ -0,0 +1,15 @@
+package examples7;
+
+public class ClassExamples {
+	String name ;
+	  double balance;
+	  String address;
+	 double intrestRate;
+	 String phone;
+ String pin;
+	 String email;
+String birthDay;
+	
+	
+
+}
